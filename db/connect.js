f2a22e0ac9f482b2fddad07c44314a3ec5e362ad
Raya Basu <108759478+RayaBasu01@users.noreply.github.com>
2023-05-27 02:32:38 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,4 @@
 const mongoose =require("mongoose");
-// uri = "mongodb+srv://*********@kinarastudentapi.26t7kep.mongodb.net/KinaraStudentApi?retryWrites=true&w=majority" ;
-
 
 const connDb=(uri)=>{
     console.log("DataBase connected");
