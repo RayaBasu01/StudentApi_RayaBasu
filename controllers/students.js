1fec96877fc89aa7c8fb3a584b00a883ff6052e4
Raya Basu <108759478+RayaBasu01@users.noreply.github.com>
2023-05-27 15:15:46 +0530

    some edits

--- controllers/students.js
+++ controllers/students.js
@@ -27,13 +27,13 @@ const getAllStudents = async(req,res)=>{
     }
 
 
-    /* API Pagination */
-    // let page = Number(req.query.page) || 1;
-    // let limit = Number(req.query.limit) ||10;
+    
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) ||10;
 
-    // let skip = (page -  1 )*limit;
+    let skip = (page -  1 )*limit;
 
-    // data=data.skip(skip).limit(limit);
+    data=data.skip(skip).limit(limit);
 
 
 
@@ -70,17 +70,18 @@ const getAllStudentsTesting = async(req,res)=>{
         data=data.sort(sorted);
     }
        
-    let page = Number(req.query.page) || 1;
-    let limit = Number(req.query.limit) ||10;
+    /* API Pagination */
+    // let page = Number(req.query.page) || 1;
+    // let limit = Number(req.query.limit) ||10;
 
-    let skip = (page -  1 )*limit;
+    // let skip = (page -  1 )*limit;
 
-    data=data.skip(skip).limit(limit);
+    // data=data.skip(skip).limit(limit);
 
 
     const StudentData= await data; 
     console.log(req.query);
-    res.status(200).json({StudentData})
+    res.status(200).json({StudentData,nbHits:StudentData.length})
 };
 
 module.exports ={getAllStudents, getAllStudentsTesting}
